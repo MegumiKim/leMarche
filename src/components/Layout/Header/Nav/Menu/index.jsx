20302b3f100bb1ad655c351aaa707ca5f7a08717
Megumi Kimura <kimuramegumi@gmail.com>
2023-05-06 16:24:15 +0200

    Toggle menu button

--- src/components/Layout/Header/Nav/Menu/index.jsx
+++ src/components/Layout/Header/Nav/Menu/index.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { FaBars } from "react-icons/fa";
+import StyledMenu from "./Menu.styled";
 
 export default function Menu() {
-  return <FaBars />;
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <StyledMenu onClick={() => setShowMenu(!showMenu)}>
+      <FaBars />
+      {showMenu && (
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      )}
+    </StyledMenu>
+  );
 }
