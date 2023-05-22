4e9b00b04a0d69a417e0c7e3e6961ceac627b305
Megumi Kimura <kimuramegumi@gmail.com>
2023-05-22 08:48:36 +0200

    Move back the redirects to the root

--- src/components/Layout/Header/Nav/CartIcon/index.jsx
+++ src/components/Layout/Header/Nav/CartIcon/index.jsx
@@ -1,15 +1,14 @@
-import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import CartStyled from "./CartIcon.styled";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../../../../context/Context";
+import { FaShoppingCart } from "react-icons/fa";
+import CartStyled from "./CartIcon.styled";
 
 export default function Cart() {
   const GlobalState = useContext(CartContext);
   const state = GlobalState["state"];
   const numberOfItem = state.item.length;
 
-  console.log(numberOfItem);
   return (
     <CartStyled>
       <Link to="/Cart">
