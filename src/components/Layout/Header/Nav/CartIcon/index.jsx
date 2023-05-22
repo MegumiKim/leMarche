ef89f0d09cd75b90d0f1e5c635c6e260b39b36fc
Megumi Kimura <kimuramegumi@gmail.com>
2023-05-22 09:28:48 +0200

    reactivate number of item in cart

--- src/components/Layout/Header/Nav/CartIcon/index.jsx
+++ src/components/Layout/Header/Nav/CartIcon/index.jsx
@@ -7,13 +7,13 @@ import CartStyled from "./CartIcon.styled";
 export default function Cart() {
   const GlobalState = useContext(CartContext);
   const state = GlobalState.state;
-  // const numberOfItem = state.item.length;
+  const numberOfItem = state.item.length;
 
   return (
     <CartStyled>
       <Link to="/Cart">
         <FaShoppingCart />
-        {/* {numberOfItem > 0 && <span>{numberOfItem}</span>} */}
+        {numberOfItem > 0 && <span>{numberOfItem}</span>}
       </Link>
     </CartStyled>
   );
